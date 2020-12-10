6e1a56edd7f04213815e4f35c4307da4ae7e311a
muhammedfurkan <byazade@yandex.com>
2020-12-10 13:57:58 +0300

    hmm

--- userbot/inlinebot.py
+++ userbot/inlinebot.py
@@ -11,150 +11,150 @@ from telethon import custom, events
 from userbot import bot, tgbot
 from userbot.util import humanbytes
 
-if Config.TG_BOT_USER_NAME_BF_HER:
-    @tgbot.on(events.InlineQuery)
-    async def inline_handler(event):
-        me = await bot.get_me()
-        builder = event.builder
-        result = None
-        query = event.text
-        if not os.path.isdir(Config.TMP_DOWNLOAD_DIRECTORY):
-            os.makedirs(Config.TMP_DOWNLOAD_DIRECTORY)
-        if query.startswith("ytdl"):
-            # input format should be ytdl URL
-            p = re.compile("ytdl (.*)")
-            r = p.search(query)
-            ytdl_url = r.group(1).strip()
-            print(ytdl_url)
-            if ytdl_url.startswith("http"):
-                command_to_exec = [
-                    "youtube-dl",
-                    "--no-warnings",
-                    "--youtube-skip-dash-manifest",
-                    "-j",
-                    ytdl_url
-                ]
-                logging.info(command_to_exec)
-                process = await asyncio.create_subprocess_exec(
-                    *command_to_exec,
-                    # stdout must a pipe to be accessible as process.stdout
-                    stdout=asyncio.subprocess.PIPE,
-                    stderr=asyncio.subprocess.PIPE,
+
+@tgbot.on(events.InlineQuery)
+async def inline_handler(event):
+    me = await bot.get_me()
+    builder = event.builder
+    result = None
+    query = event.text
+    if not os.path.isdir(Config.TMP_DOWNLOAD_DIRECTORY):
+        os.makedirs(Config.TMP_DOWNLOAD_DIRECTORY)
+    if query.startswith("ytdl"):
+        # input format should be ytdl URL
+        p = re.compile("ytdl (.*)")
+        r = p.search(query)
+        ytdl_url = r.group(1).strip()
+        print(ytdl_url)
+        if ytdl_url.startswith("http"):
+            command_to_exec = [
+                "youtube-dl",
+                "--no-warnings",
+                "--youtube-skip-dash-manifest",
+                "-j",
+                ytdl_url
+            ]
+            logging.info(command_to_exec)
+            process = await asyncio.create_subprocess_exec(
+                *command_to_exec,
+                # stdout must a pipe to be accessible as process.stdout
+                stdout=asyncio.subprocess.PIPE,
+                stderr=asyncio.subprocess.PIPE,
+            )
+            # Wait for the subprocess to finish
+            stdout, stderr = await process.communicate()
+            e_response = stderr.decode().strip()
+            # logger.info(e_response)
+            t_response = stdout.decode().strip()
+            logging.info(command_to_exec)
+            if e_response:
+                error_message = e_response.replace(
+                    "please report this issue on https://yt-dl.org/bug . Make sure you are using the latest version; see  https://yt-dl.org/update  on how to update. Be sure to call youtube-dl with the --verbose flag and include its complete output.", "")
+                # throw error
+                result = builder.article(
+                    "YTDL Errors © @TelethonUserBot",
+                    text=f"{error_message} Powered by @TelethonUserBot",
+                    link_preview=False
                 )
-                # Wait for the subprocess to finish
-                stdout, stderr = await process.communicate()
-                e_response = stderr.decode().strip()
-                # logger.info(e_response)
-                t_response = stdout.decode().strip()
-                logging.info(command_to_exec)
-                if e_response:
-                    error_message = e_response.replace(
-                        "please report this issue on https://yt-dl.org/bug . Make sure you are using the latest version; see  https://yt-dl.org/update  on how to update. Be sure to call youtube-dl with the --verbose flag and include its complete output.", "")
-                    # throw error
-                    result = builder.article(
-                        "YTDL Errors © @TelethonUserBot",
-                        text=f"{error_message} Powered by @TelethonUserBot",
-                        link_preview=False
-                    )
-                elif t_response:
-                    x_reponse = t_response
-                    if "\n" in x_reponse:
-                        x_reponse, _ = x_reponse.split("\n")
-                    response_json = json.loads(x_reponse)
-                    save_ytdl_json_path = Config.TMP_DOWNLOAD_DIRECTORY + \
-                        "/" + "YouTubeDL" + ".json"
-                    with open(save_ytdl_json_path, "w", encoding="utf8") as outfile:
-                        json.dump(response_json, outfile, ensure_ascii=False)
-                    # logger.info(response_json)
-                    inline_keyboard = []
-                    duration = None
-                    if "duration" in response_json:
-                        duration = response_json["duration"]
-                    if "formats" in response_json:
-                        for formats in response_json["formats"]:
-                            format_id = formats.get("format_id")
-                            format_string = formats.get("format_note")
-                            if format_string is None:
-                                format_string = formats.get("format")
-                            format_ext = formats.get("ext")
-                            approx_file_size = ""
-                            if "filesize" in formats:
-                                approx_file_size = humanbytes(
-                                    formats["filesize"])
-                            cb_string_video = "ytdl|{}|{}|{}".format(
-                                "video", format_id, format_ext)
-                            if format_string is not None:
-                                ikeyboard = [
-                                    custom.Button.inline(
-                                        " " + format_ext + " video [" + format_string +
-                                        "] ( " +
-                                        approx_file_size + " )",
-                                        data=(cb_string_video)
-                                    )
-                                ]
-                            else:
-                                # special weird case :\
-                                ikeyboard = [
-                                    custom.Button.inline(
-                                        " " + approx_file_size + " ",
-                                        data=cb_string_video
-                                    )
-                                ]
-                            inline_keyboard.append(ikeyboard)
-                        if duration is not None:
-                            cb_string_64 = "ytdl|{}|{}|{}".format(
-                                "audio", "64k", "mp3")
-                            cb_string_128 = "ytdl|{}|{}|{}".format(
-                                "audio", "128k", "mp3")
-                            cb_string = "ytdl|{}|{}|{}".format(
-                                "audio", "320k", "mp3")
-                            inline_keyboard.append([
-                                custom.Button.inline(
-                                    "MP3 " + "(" + "64 kbps" + ")", data=cb_string_64
-                                ),
-                                custom.Button.inline(
-                                    "MP3 " + "(" + "128 kbps" + ")", data=cb_string_128
-                                )
-                            ])
-                            inline_keyboard.append([
-                                custom.Button.inline(
-                                    "MP3 " + "(" + "320 kbps" + ")", data=cb_string
-                                )
-                            ])
-                    else:
-                        format_id = response_json["format_id"]
-                        format_ext = response_json["ext"]
+            elif t_response:
+                x_reponse = t_response
+                if "\n" in x_reponse:
+                    x_reponse, _ = x_reponse.split("\n")
+                response_json = json.loads(x_reponse)
+                save_ytdl_json_path = Config.TMP_DOWNLOAD_DIRECTORY + \
+                    "/" + "YouTubeDL" + ".json"
+                with open(save_ytdl_json_path, "w", encoding="utf8") as outfile:
+                    json.dump(response_json, outfile, ensure_ascii=False)
+                # logger.info(response_json)
+                inline_keyboard = []
+                duration = None
+                if "duration" in response_json:
+                    duration = response_json["duration"]
+                if "formats" in response_json:
+                    for formats in response_json["formats"]:
+                        format_id = formats.get("format_id")
+                        format_string = formats.get("format_note")
+                        if format_string is None:
+                            format_string = formats.get("format")
+                        format_ext = formats.get("ext")
+                        approx_file_size = ""
+                        if "filesize" in formats:
+                            approx_file_size = humanbytes(
+                                formats["filesize"])
                         cb_string_video = "ytdl|{}|{}|{}".format(
                             "video", format_id, format_ext)
+                        if format_string is not None:
+                            ikeyboard = [
+                                custom.Button.inline(
+                                    " " + format_ext + " video [" + format_string +
+                                    "] ( " +
+                                    approx_file_size + " )",
+                                    data=(cb_string_video)
+                                )
+                            ]
+                        else:
+                            # special weird case :\
+                            ikeyboard = [
+                                custom.Button.inline(
+                                    " " + approx_file_size + " ",
+                                    data=cb_string_video
+                                )
+                            ]
+                        inline_keyboard.append(ikeyboard)
+                    if duration is not None:
+                        cb_string_64 = "ytdl|{}|{}|{}".format(
+                            "audio", "64k", "mp3")
+                        cb_string_128 = "ytdl|{}|{}|{}".format(
+                            "audio", "128k", "mp3")
+                        cb_string = "ytdl|{}|{}|{}".format(
+                            "audio", "320k", "mp3")
                         inline_keyboard.append([
                             custom.Button.inline(
-                                "video",
-                                data=cb_string_video
+                                "MP3 " + "(" + "64 kbps" + ")", data=cb_string_64
+                            ),
+                            custom.Button.inline(
+                                "MP3 " + "(" + "128 kbps" + ")", data=cb_string_128
                             )
                         ])
-                    result = builder.article(
-                        "YouTube © UserBOT",
-                        text=f"{ytdl_url} powered by UserBOT",
-                        buttons=inline_keyboard,
-                        link_preview=True
-                    )
-        else:
-            result = builder.article(
-                "© UserBOT",
-                text=(
-                    "Try UserBOT\n"
-                    "You can log-in as Bot or User and do many cool things with your Telegram account.\n\n"
-                    "All instaructions to run UserBOT in your PC has been explained in https://github.com/muhammedfurkan/TelethonUserBot"
-                ),
-                buttons=[
-                    [custom.Button.url("Join the Channel", "https://telegram.dog/telethon"), custom.Button.url(
-                        "Join the Group", "tg://some_unsupported_feature")],
-                    [custom.Button.url(
-                        "Source Code", "tg://some_unsupported_feature")]
-                ],
-                link_preview=False
-            )
-        await event.answer([result] if result else None)
+                        inline_keyboard.append([
+                            custom.Button.inline(
+                                "MP3 " + "(" + "320 kbps" + ")", data=cb_string
+                            )
+                        ])
+                else:
+                    format_id = response_json["format_id"]
+                    format_ext = response_json["ext"]
+                    cb_string_video = "ytdl|{}|{}|{}".format(
+                        "video", format_id, format_ext)
+                    inline_keyboard.append([
+                        custom.Button.inline(
+                            "video",
+                            data=cb_string_video
+                        )
+                    ])
+                result = builder.article(
+                    "YouTube © UserBOT",
+                    text=f"{ytdl_url} powered by UserBOT",
+                    buttons=inline_keyboard,
+                    link_preview=True
+                )
+    else:
+        result = builder.article(
+            "© UserBOT",
+            text=(
+                "Try UserBOT\n"
+                "You can log-in as Bot or User and do many cool things with your Telegram account.\n\n"
+                "All instaructions to run UserBOT in your PC has been explained in https://github.com/muhammedfurkan/TelethonUserBot"
+            ),
+            buttons=[
+                [custom.Button.url("Join the Channel", "https://telegram.dog/telethon"), custom.Button.url(
+                    "Join the Group", "tg://some_unsupported_feature")],
+                [custom.Button.url(
+                    "Source Code", "tg://some_unsupported_feature")]
+            ],
+            link_preview=False
+        )
+    await event.answer([result] if result else None)
 
 
 def paginate_help(page_number, loaded_plugins, prefix):
