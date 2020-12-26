d6231d302d7b2b4ac9608c4aac1cbfb93fbf544d
hamitdurmuss <60671300+hamitdurmuss@users.noreply.github.com>
2020-12-26 21:44:14 +0300

    Update update.py

--- userbot/modules/update.py
+++ userbot/modules/update.py
@@ -46,7 +46,7 @@ async def upstream(ups):
     try:
         txt = "`Oops.. Updater cannot continue due to "
         txt += "some problems occured`\n\n**LOGTRACE:**\n"
-        repo = Repo()
+        repo = Repo(search_parent_directories=True)
     except NoSuchPathError as error:
         await ups.edit(f'{txt}\n`directory {error} is not found`')
         repo.__del__()
