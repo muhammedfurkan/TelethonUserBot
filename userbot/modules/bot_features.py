b321053ac5331cd228885c62856123b03b00dc52
muhammedfurkan <byazade@yandex.com>
2020-12-26 20:06:58 +0300

    new sangmata

--- userbot/modules/bot_features.py
+++ userbot/modules/bot_features.py
@@ -12,35 +12,53 @@ logger = logging.getLogger(__name__)
 
 
 @bot.on(admin_cmd(pattern=("sg ?(.*)")))
-async def _(event):
-    if event.fwd_from:
+async def lastname(steal):
+    if steal.fwd_from:
         return
-    if not event.reply_to_msg_id:
-        await event.edit("```Reply to any user message.```")
+    if not steal.reply_to_msg_id:
+        await steal.edit("```Reply to any user message.```")
         return
-    reply_message = await event.get_reply_message()
-    if not reply_message.text:
-        await event.edit("```reply to text message```")
-        return
+    message = await steal.get_reply_message()
     chat = "@SangMataInfo_bot"
-    sender = reply_message.sender
-    if sender.bot:
-        await event.edit("```Reply to actual users message.```")
+    user_id = message.sender.id
+    id = f"/search_id {user_id}"
+    if message.sender.bot:
+        await steal.edit("```Reply to actual users message.```")
         return
-    await event.edit("```Processing```")
-    async with event.client.conversation(chat) as conv:
-        try:
-            response = conv.wait_event(events.NewMessage(
-                incoming=True, from_users=461843263))
-            await event.client.forward_messages(chat, reply_message)
-            response = await response
-        except YouBlockedUserError:
-            await event.reply("```Please unblock @sangmatainfo_bot and try again```")
-            return
-        if response.text.startswith("Forward"):
-            await event.edit("```can you kindly disable your forward privacy settings for good?```")
-        else:
-            await event.edit(f"{response.message.message}")
+    await steal.edit("```Sit tight while I steal some data from NASA```")
+    try:
+        async with bot.conversation(chat) as conv:
+            try:
+                msg = await conv.send_message(id)
+                r = await conv.get_response()
+                response = await conv.get_response()
+            except YouBlockedUserError:
+                await steal.reply(
+                    "```Please unblock @sangmatainfo_bot and try again```"
+                )
+                return
+            if r.text.startswith("Name"):
+                respond = await conv.get_response()
+                await steal.edit(f"{r.message}")
+                await steal.client.delete_messages(
+                    conv.chat_id, [msg.id, r.id, response.id, respond.id]
+                )
+                return
+            if response.text.startswith("No records") or r.text.startswith(
+                "No records"
+            ):
+                await steal.edit("```No records found for this user```")
+                await steal.client.delete_messages(
+                    conv.chat_id, [msg.id, r.id, response.id]
+                )
+                return
+            respond = await conv.get_response()
+            await steal.edit(f"{response.message}")
+            await steal.client.delete_messages(
+                conv.chat_id, [msg.id, r.id, response.id, respond.id]
+            )
+    except TimeoutError:
+        return await steal.edit("`Error: `@SangMataInfo_bot` is not responding!.`")
 
 
 @bot.on(admin_cmd(pattern=("fakemail ?(.*)")))
