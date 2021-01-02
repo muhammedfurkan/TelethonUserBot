57d86d8c9a68a9bd815d4ca8ef6f4d92b5ce9de1
muhammedfurkan <byazade@yandex.com>
2021-01-02 22:08:31 +0300

    upload plugin

--- userbot/modules/_help.py
+++ userbot/modules/_help.py
@@ -8,8 +8,10 @@
 ◇ to know syntax
 ◆ `.syntax` <plugin name>
 """
+import asyncio
 import logging
 import sys
+from datetime import datetime
 
 from telethon import __version__, functions
 from userbot import bot
@@ -18,6 +20,8 @@ from userbot.util import admin_cmd
 logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
                     level=logging.WARNING)
 logger = logging.getLogger(__name__)
+
+DELETE_TIMEOUT = 5
 
 
 @bot.on(admin_cmd(pattern="helpme ?(.*)", allow_sudo=True))
@@ -55,3 +59,26 @@ async def _(event):
     result = result.stringify()
     logger.info(result)
     await event.edit("""Telethon UserBot powered by @By_Azade""")
+
+
+@bot.on(admin_cmd(pattern="send plugin (?P<shortname>\w+)$"))  # pylint:disable=E0602
+async def send_plug_in(event):
+    if event.fwd_from:
+        return
+    message_id = event.message.id
+    input_str = event.pattern_match["shortname"]
+    the_plugin_file = "./userbot/modules/{}.py".format(
+        input_str)
+    start = datetime.now()
+    await bot.send_file(
+        event.chat_id,
+        the_plugin_file,
+        force_document=True,
+        allow_cache=False,
+        reply_to=message_id
+    )
+    end = datetime.now()
+    time_taken_in_ms = (end - start).seconds
+    await event.edit("Uploaded {} in {} seconds".format(input_str, time_taken_in_ms))
+    await asyncio.sleep(DELETE_TIMEOUT)
+    await event.delete()
