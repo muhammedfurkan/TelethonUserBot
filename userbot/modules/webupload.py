90a7d63a96cc42a82167cd805306665f62d9088c
muhammedfurkan <byazade@yandex.com>
2021-01-08 20:32:30 +0300

    again gofile.io support

--- userbot/modules/webupload.py
+++ userbot/modules/webupload.py
@@ -9,7 +9,7 @@ import os
 import aiohttp
 from sample_config import Config
 from userbot import bot
-from userbot.util import admin_cmd
+from userbot.util import admin_cmd, run_command
 
 logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
                     level=logging.WARNING)
@@ -74,3 +74,47 @@ async def _(event):
         # assuming, the return values won't be longer than
         # 4096 characters
         await event.edit(t_response)
+
+
+@bot.on(admin_cmd(pattern="goup ?(.*)"))
+async def _(event):
+    await event.edit("processing ...")
+    input_str = event.pattern_match.group(1)
+    if input_str:
+        file_name = input_str
+    else:
+        reply = await event.get_reply_message()
+        file_name = await event.client.download_media(
+            reply.media,
+            Config.TMP_DOWNLOAD_DIRECTORY
+        )
+    # a dictionary containing the shell commands
+    CMD_WEB = {
+        "goup": "curl -F \"email=furki@telegmail.com\" -F \"file=@{full_file_path}\" https://srv-store4.gofile.io/uploadFile",
+    }
+    filename = os.path.basename(file_name)
+    try:
+        selected_one = CMD_WEB["goup"].format(
+            full_file_path=file_name,
+            bare_local_name=filename
+        )
+    except KeyError:
+        await event.edit("Invalid selected Transfer")
+        return
+    cmd = selected_one
+    # start the subprocess $SHELL
+    t_response, e_response = await run_command(cmd)
+    if t_response:
+        try:
+            t_response = json.dumps(json.loads(
+                t_response), sort_keys=True, indent=4)
+        except Exception as e:
+            # some sites don't return valid JSONs
+            pass
+        # assuming, the return values won't be longer than
+        # 4096 characters
+        # print(t_response)
+        resp = json.loads(t_response)
+        msg = "Uploaded Url: {}".format(
+            "https://gofile.io/d/"+resp['data']['code'])
+        await event.edit(msg)
