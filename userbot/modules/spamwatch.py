71cb23170d173b8f74fc088debcbf81ceebcd4e3
muhammedfurkan <byazade@yandex.com>
2021-09-01 10:59:40 +0300

    update

--- userbot/modules/spamwatch.py
+++ userbot/modules/spamwatch.py
@@ -2,14 +2,14 @@
 
 import logging
 
-import spamwatch
+from sample_config import Config
 from telethon import events
 from telethon.errors import BadRequestError
 from telethon.tl.functions.channels import EditBannedRequest
 from telethon.tl.types import ChatBannedRights
-
-from sample_config import Config
 from userbot import bot
+
+import spamwatch
 
 logging.basicConfig(
     format="[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s", level=logging.WARNING
@@ -33,39 +33,41 @@ BANNED_RIGHTS = ChatBannedRights(
 
 @bot.on(events.ChatAction(func=lambda e: e.is_group))
 async def spam_watch_(event):
+
+    if not Config.SPAM_WATCHAPI:
+        return
     chat = await event.get_chat()
-    if Config.SPAM_WATCHAPI:
-        client = spamwatch.Client(Config.SPAM_WATCH_API)
-        user = await event.get_user()
+    client = spamwatch.Client(Config.SPAM_WATCH_API)
+    user = await event.get_user()
+    try:
+        if (
+            not chat.admin_rights
+            and not chat.creator
+            or not event.user_joined
+            and not event.user_added
+        ):
+            return ""
         try:
-            if (
-                not chat.admin_rights
-                and not chat.creator
-                or not event.user_joined
-                and not event.user_added
-            ):
-                return ""
-            try:
-                ban = client.get_ban(user.id)
-                if ban:
-                    await event.client(
-                        EditBannedRequest(
-                            event.chat_id, user.id, BANNED_RIGHTS)
-                    )
-                else:
-                    return
-            except AttributeError:
-                return
-            except BadRequestError:
-                return
-            except ValueError:
-                return
-            if ENABLE_LOG:
-                await event.client.send_message(
-                    Config.SPAM_WATCH_LOG_CHANNEL,
-                    "#SPAMWATCH_BAN\n"
-                    f"USER: [{user.first_name}](tg://user?id={user.id})\n"
-                    f"CHAT: {event.chat.title}(`{event.chat_id}`)",
+            ban = client.get_ban(user.id)
+            if ban:
+                await event.client(
+                    EditBannedRequest(
+                        event.chat_id, user.id, BANNED_RIGHTS)
                 )
+            else:
+                return
         except AttributeError:
             return
+        except BadRequestError:
+            return
+        except ValueError:
+            return
+        if ENABLE_LOG:
+            await event.client.send_message(
+                Config.SPAM_WATCH_LOG_CHANNEL,
+                "#SPAMWATCH_BAN\n"
+                f"USER: [{user.first_name}](tg://user?id={user.id})\n"
+                f"CHAT: {event.chat.title}(`{event.chat_id}`)",
+            )
+    except AttributeError:
+        return
