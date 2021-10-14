86bf79058cab7c6607405cb64af093aaf4b80dc3
muhammedfurkan <byazade@yandex.com>
2021-10-14 19:52:53 +0300

    Revert "hmm"
    This reverts commit 27318bd191b4f55aac212b1ac942221802d2ff2f.

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -6,16 +6,13 @@
 """ Userbot initialization. """
 
 
+from mongoengine import connect
 from sample_config import Config
+from telemongo import MongoSession
 from telethon import TelegramClient
-from telethon.sessions import StringSession
 
-if Config.HU_STRING_SESSION:
-    bot = TelegramClient(StringSession(
-        Config.HU_STRING_SESSION), Config.APP_ID, Config.API_HASH)
-else:
-    bot = TelegramClient("userbot", Config.APP_ID, Config.API_HASH)
+connect('telegramsession', host=Config.MONGO_DB_URI)
+session = MongoSession('telegramsession', host=Config.MONGO_DB_URI)
 
-if Config.TG_BOT_TOKEN_BF_HER:
-    tgbot = TelegramClient("bot", Config.APP_ID, Config.API_HASH).start(
-        bot_token=Config.TG_BOT_TOKEN_BF_HER)
+if session:
+    bot = TelegramClient(session, Config.APP_ID, Config.API_HASH)
