c74190c140db1a56dde0fe234968f64208ba3ae3
muhammedfurkan <byazade@yandex.com>
2021-10-23 23:41:29 +0300

    ._.

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -8,6 +8,7 @@ if Config.HU_STRING_SESSION:
 else:
     bot = TelegramClient("userbot", Config.APP_ID, Config.API_HASH)
 
+
 if Config.TG_BOT_TOKEN_BF_HER:
     tgbot = TelegramClient("bot", Config.APP_ID, Config.API_HASH).start(
         bot_token=Config.TG_BOT_TOKEN_BF_HER)
