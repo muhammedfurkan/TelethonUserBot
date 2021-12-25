37691ac9144db08155a2b65043c9bd80ce6dc340
muhammedfurkan <byazade@yandex.com>
2021-12-25 11:31:32 +0300

    disable inline bot cuz not working(investigation this issue)

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -9,6 +9,6 @@ if Config.HU_STRING_SESSION:
 else:
     bot = TelegramClient("telethonuserbot", Config.APP_ID, Config.API_HASH)
 
-if Config.TG_BOT_TOKEN_BF_HER:
-    tgbot = TelegramClient("bot", Config.APP_ID, Config.API_HASH).start(
-        bot_token=Config.TG_BOT_TOKEN_BF_HER)
+# if Config.TG_BOT_TOKEN_BF_HER:
+#     tgbot = TelegramClient("bot", Config.APP_ID, Config.API_HASH).start(
+#         bot_token=Config.TG_BOT_TOKEN_BF_HER)
