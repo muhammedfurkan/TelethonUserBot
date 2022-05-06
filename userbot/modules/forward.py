7a74ed19f96fa03896d824d9c6ed7a0c9c9bb523
M.Furkan <byazade@yandex.com>
2022-05-06 11:16:47 +0300

    new plugin for own purpose

---
+++ userbot/modules/forward.py
@@ -0,0 +1,24 @@
+import logging
+
+from userbot import bot
+from userbot.util import admin_cmd
+
+logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
+                    level=logging.WARNING)
+logger = logging.getLogger(__name__)
+
+
+@bot.on(admin_cmd(func=lambda e: e.media))
+async def _(event):
+    media_mime = ["application/zip", "application/x-rar-compressed", "application/x-rar", "application/x-7z-compressed","application/pdf"]
+    try:
+        e = await event.client.get_entity("DosyaAraBot")
+    except Exception as e:
+        await event.client.send_message("me", str(e))
+    else:
+        await event.client.send_message(
+            entity="DosyaAraBot",
+            message="**DosyaAraBot**\n\n",
+            file=event.media if event.message.media.document.mime_type in media_mime else event.message,
+        )
+
