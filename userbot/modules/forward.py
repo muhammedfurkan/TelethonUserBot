0a9a15579433415c877be19146bbff6cd882ef55
M.Furkan <byazade@yandex.com>
2022-05-06 19:09:51 +0300

    edit

--- userbot/modules/forward.py
+++ userbot/modules/forward.py
@@ -14,10 +14,11 @@ async def _(event):
     media_mime = ["application/zip", "application/x-rar-compressed",
                   "application/x-rar", "application/x-7z-compressed", "application/pdf"]
     try:
-        await event.client.send_message(
-            entity="DosyaAraBot",
-            message="**DosyaAraBot**\n\n",
-            file=event.media if event.message.media.document.mime_type in media_mime else event.media,
-        )
+        if event.media.document.mime_type in media_mime:
+            await event.client.send_message(
+                entity="DosyaAraBot",
+                message="**DosyaAraBot**\n\n",
+                file=event.media,
+            )
     except AttributeError:
         return
