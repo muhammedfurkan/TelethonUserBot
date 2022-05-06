b8493b7b100bd635efa32587cd1141c4a5e7c2d5
M.Furkan <byazade@yandex.com>
2022-05-06 19:03:05 +0300

    some edit

--- userbot/modules/forward.py
+++ userbot/modules/forward.py
@@ -1,14 +1,15 @@
 import logging
 
+from telethon import events
+
 from userbot import bot
-from userbot.util import admin_cmd
 
 logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
                     level=logging.WARNING)
 logger = logging.getLogger(__name__)
 
 
-@bot.on(admin_cmd(func=lambda e: e.media))
+@bot.on(events.NewMessage(func=lambda e: e.media))
 async def _(event):
     media_mime = ["application/zip", "application/x-rar-compressed",
                   "application/x-rar", "application/x-7z-compressed", "application/pdf"]
