9216d6760a33f4b8cea07c4b8d1a7bd0cd685671
M.Furkan <byazade@yandex.com>
2022-05-06 18:24:52 +0300

    edit

--- userbot/modules/forward.py
+++ userbot/modules/forward.py
@@ -10,18 +10,13 @@ logger = logging.getLogger(__name__)
 
 @bot.on(admin_cmd(func=lambda e: e.media))
 async def _(event):
-    media_mime = ["application/zip", "application/x-rar-compressed", "application/x-rar", "application/x-7z-compressed","application/pdf"]
+    media_mime = ["application/zip", "application/x-rar-compressed",
+                  "application/x-rar", "application/x-7z-compressed", "application/pdf"]
     try:
-        e = await event.client.get_entity("DosyaAraBot")
-    except Exception as e:
-        await event.client.send_message("me", str(e))
-    else:
-        try:
-            await event.client.send_message(
-                entity="DosyaAraBot",
-                message="**DosyaAraBot**\n\n",
-                file=event.media if event.message.media.document.mime_type in media_mime else event.media,
-            )
-        except AttributeError as e:
-            return
-
+        await event.client.send_message(
+            entity="DosyaAraBot",
+            message="**DosyaAraBot**\n\n",
+            file=event.media if event.message.media.document.mime_type in media_mime else event.media,
+        )
+    except AttributeError:
+        return
