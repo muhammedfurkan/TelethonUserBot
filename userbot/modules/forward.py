c9efc0b96e82d586eb0b32a6d8d0591d3f258427
M.Furkan <muhammedfurkan.ersoy@arneca.com>
2022-08-30 10:51:14 +0300

    update

--- userbot/modules/forward.py
+++ userbot/modules/forward.py
@@ -1,18 +1,25 @@
+import asyncio
 import logging
 
 from telethon import events
-
+from telethon.errors import FloodWaitError
 from userbot import bot
 
-logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
-                    level=logging.WARNING)
+logging.basicConfig(
+    format="[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s", level=logging.WARNING
+)
 logger = logging.getLogger(__name__)
 
 
 @bot.on(events.NewMessage(func=lambda e: e.document, incoming=True))
 async def _(event):
-    media_mime = ["application/zip", "application/x-rar-compressed",
-                  "application/x-rar", "application/x-7z-compressed", "application/pdf"]
+    media_mime = [
+        "application/zip",
+        "application/x-rar-compressed",
+        "application/x-rar",
+        "application/x-7z-compressed",
+        "application/pdf",
+    ]
     try:
         if event.media.document.mime_type in media_mime:
             await event.client.send_message(
@@ -22,3 +29,5 @@ async def _(event):
             )
     except AttributeError:
         return
+    except FloodWaitError as e:
+        await asyncio.sleep(e.seconds)
